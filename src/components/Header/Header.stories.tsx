530886cbe1d2130250ba37767cad903d561b1dd8
Shaul Ben david <Shaulbd9@gmail.com>
2023-05-28 19:45:28 +0300

    feat(header): header storybook

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactFocusLock from 'react-focus-lock';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import Header from './Header';
 
-export default {
+const meta: Meta<typeof Header> = {
   title: 'Layout/Header',
   component: Header,
-} as ComponentMeta<typeof Header>;
+};
 
-const Template: ComponentStory<typeof Header> = () => (
-  <ReactFocusLock>
-    <Header />
-  </ReactFocusLock>
-);
-export const Playground = Template.bind({});
-Playground.args = {};
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Primary: Story = {
+  render: () => <Header />,
+};
