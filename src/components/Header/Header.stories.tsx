394155b05b2ece8b9dc9f53926168196c2586f74
Shaul Ben david <Shaulbd9@gmail.com>
2023-06-05 14:26:56 +0300

    fix(storybook): fixing storybook when using position fixed

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import Header from './Header';
+
+const styles: CSSProperties = {
+  transform: 'scale(1)',
+  height: '100vh',
+  position: 'relative',
+};
 
 const meta: Meta<typeof Header> = {
   title: 'Layout/Header',
   component: Header,
   tags: ['autodocs'],
+  decorators: [(storyFn) => <div style={styles}>{storyFn()}</div>],
 };
 
 export default meta;
--- src/components/Header/SideNavigation/SideNavigation.stories.tsx
+++ src/components/Header/SideNavigation/SideNavigation.stories.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import SideNavigation from './SideNavigation';
+
+const styles: CSSProperties = {
+  transform: 'scale(1)',
+  height: '100vh',
+  position: 'relative',
+};
 
 const meta: Meta<typeof SideNavigation> = {
   title: 'Layout/SideNavigation',
   component: SideNavigation,
   tags: ['autodocs'],
+  decorators: [(storyFn) => <div style={styles}>{storyFn()}</div>],
 };
 
 export default meta;
