38f2363d98f1217d7789fc253c2a666a686ae8b6
Shaul Ben david <Shaulbd9@gmail.com>
2023-06-26 20:37:35 +0300

    refactor(sociallinks): config file

---
+++ src/components/Footer/SocialIconsLinks/SocialIconsLinks.config.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiOutlineTwitter,
+} from 'react-icons/ai';
+import theme from '@/src/styles/tailwind.theme';
+import { SocialEnum } from '@/src/constants/outSource';
+
+const socialIconsConfig: Record<SocialEnum, JSX.Element> = {
+  [SocialEnum.GITHUB]: (
+    <AiFillGithub aria-hidden fill={theme.secondary} width={24} height={24} />
+  ),
+  [SocialEnum.INSTAGRAM]: (
+    <AiFillInstagram
+      aria-hidden
+      fill={theme.secondary}
+      width={24}
+      height={24}
+    />
+  ),
+  [SocialEnum.TWITTER]: (
+    <AiOutlineTwitter
+      aria-hidden
+      fill={theme.secondary}
+      width={24}
+      height={24}
+    />
+  ),
+};
+
+export default socialIconsConfig;
