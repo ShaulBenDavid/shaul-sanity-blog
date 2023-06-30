a64f92e832e9bc3d3661caaa91befd41f5f90287
Shaul Ben david <Shaulbd9@gmail.com>
2023-06-30 17:25:43 +0300

    feat(axios): adding interceptor and configure the app providers

--- src/api/http.service.ts
+++ src/api/http.service.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const config: AxiosRequestConfig = {
   url: process.env.NEXT_PUBLIC_DEV_WIZARD_URL,
@@ -6,8 +6,10 @@ const config: AxiosRequestConfig = {
   withCredentials: true,
 };
 
-const dwInstance = axios.create(config);
+export const dwInstance: AxiosInstance = axios.create(config);
 
-export const apiMethod = <T>(options: AxiosRequestConfig): Promise<T> => {
+export const apiMethodInstance = <T>(
+  options: AxiosRequestConfig
+): Promise<T> => {
   return dwInstance(options).then((res) => res.data);
 };
