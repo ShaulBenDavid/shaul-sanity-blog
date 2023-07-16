5a3ff3f50a87e84c24b3e1e13f482a033ee4ea83
Shaul Ben david <Shaulbd9@gmail.com>
2023-07-16 18:23:43 +0300

    feat(refresh): new refresh system

--- src/providers/query.provider.tsx
+++ src/providers/query.provider.tsx
@@ -5,13 +5,9 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { appQueryClient } from '../queries';
 
-export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  // const [queryClient] = React.useState(() => new QueryClient());
-
-  return (
-    <QueryClientProvider client={appQueryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
-};
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={appQueryClient}>
+    {children}
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
