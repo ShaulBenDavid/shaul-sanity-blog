e60692b0db2d6371790a078589d0aaa8fd1cbe1a
Shaul Ben david <Shaulbd9@gmail.com>
2023-07-21 17:51:14 +0300

    feat(login): adding metatags

--- app/(user)/sign-in/page.tsx
+++ app/(user)/sign-in/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Login from '@/src/features/Login';
+import { loginMetadata } from '@/src/metadata';
+
+export const metadata = loginMetadata;
 
 const LoginPage = () => (
   <section className="flex h-full w-full items-center justify-center">
