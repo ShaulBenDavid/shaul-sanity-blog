49a5a9040a5b1b3ecb3870b33886259225a6ff46
Shaul Ben david <Shaulbd9@gmail.com>
2023-07-22 13:18:47 +0300

    feat(dropdown): new component full accessible

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,32 @@
+import React, { PropsWithChildren, useRef } from 'react';
+import { useOnClickOutside } from '@/src/hooks';
+
+interface DropdownProps {
+  id: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Dropdown = ({
+  id,
+  isOpen,
+  onClose,
+  children,
+}: PropsWithChildren<DropdownProps>): JSX.Element => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(ref, onClose);
+
+  return (
+    <div
+      ref={ref}
+      className={`absolute h-fit w-fit rounded-md bg-white p-2 shadow-dropdown ${
+        isOpen ? 'flex' : 'hidden'
+      }`}
+      id={id}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect, RefObject } from 'react';
+
+export const useOnClickOutside = (
+  ref: RefObject<HTMLElement>,
+  cb: () => void
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      cb();
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, cb]);
+};
