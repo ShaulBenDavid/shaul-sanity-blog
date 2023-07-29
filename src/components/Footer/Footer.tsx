96942c2a673c54050e73cb1d743de9c76503f7db
Shaul Ben david <Shaulbd9@gmail.com>
2023-07-29 12:10:43 +0300

    fix(header): layouts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import Navigation from './Navigation';
 
 const Footer = () => (
   <footer className="w-full bg-white">
-    <div className="mx-auto w-[1227px] py-6 max-lg:w-[1000px] max-md:w-full max-md:px-2 tb:px-8 lg:py-8">
+    <div className="mx-auto w-[1227px] py-6 max-lg:w-[1000px] max-md:w-full max-md:px-2">
       <Navigation />
       <hr className="my-6 border-wizard-grey sm:mx-auto lg:my-8" aria-hidden />
       <div className="sm:flex sm:items-center sm:justify-between">
