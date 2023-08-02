8645ce4a0c9f02f0c60450c1061c1d168bdb5ffc
Shaul Ben david <Shaulbd9@gmail.com>
2023-08-02 21:19:49 +0300

    feat(protectedroute): new component and deploy in login

---
+++ app/(user)/sign-in/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProtectedRoute from '@/src/providers/ProtectedRoute';
+import { Routes } from '@/src/routes';
+import { Roles } from '@/src/roles';
+
+const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => (
+  <ProtectedRoute allowedRoles={[Roles.VISITOR]} defaultRoute={Routes.ROOT}>
+    {children}
+  </ProtectedRoute>
+);
+
+export default Layout;
---
+++ src/providers/ProtectedRoute.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useContext, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { AuthContext } from '@/src/context/auth';
+import { hasMatchingRole } from '../roles/roles.utils';
+import { Roles } from '../roles';
+import { Routes } from '../routes';
+
+interface ProtectedRouteProps {
+  allowedRoles: Roles[];
+  defaultRoute?: Routes;
+  children: React.ReactNode;
+}
+
+const ProtectedRoute = ({
+  allowedRoles,
+  defaultRoute = Routes.SIGN_IN,
+  children,
+}: ProtectedRouteProps): JSX.Element | null => {
+  const { roles, isAuth } = useContext(AuthContext);
+  const router = useRouter();
+
+  const isAllowed = hasMatchingRole(allowedRoles, roles);
+
+  useEffect(() => {
+    if (!isAllowed && isAuth) {
+      router.push(defaultRoute);
+    }
+  }, [router, roles, isAuth]);
+
+  return isAllowed ? <>{children}</> : <></>;
+};
+
+export default ProtectedRoute;
