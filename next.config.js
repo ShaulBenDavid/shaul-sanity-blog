34bbd3ec20fd43a4e8bf7722e7a2c1804c5f671f
Shaul Ben david <Shaulbd9@gmail.com>
2023-08-04 10:06:21 +0300

    feat(proxy): hiding backend url

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // experimental: {
-  //   appDir: true,
-  // },
   images: {
     domains: ['cdn.sanity.io'],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_DEV_WIZARD_URL}:path*`,
+      },
+    ];
   },
 
   reactStrictMode: true,
