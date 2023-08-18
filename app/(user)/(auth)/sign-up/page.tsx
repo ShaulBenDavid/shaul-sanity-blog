e59e50f80326fa7bfe1ce189631924775141aefb
Shaul Ben david <Shaulbd9@gmail.com>
2023-08-18 12:02:33 +0300

    feat(register): metadata for seo

--- app/(user)/(auth)/sign-up/page.tsx
+++ app/(user)/(auth)/sign-up/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Register from '@/src/features/Register';
+import { registerMetadata } from '@/src/metadata';
+
+export const metadata = registerMetadata;
 
 const RegisterPage = () => {
   return (
--- src/metadata/index.ts
+++ src/metadata/index.ts
@@ -1,2 +1,3 @@
 export { loginMetadata } from './login';
+export { registerMetadata } from './register';
 export { defaultMetadata } from './default';
