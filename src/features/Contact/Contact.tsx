9bf62601f9521f6526edd3d50c8587ae82660f19
Shaul Ben david <Shaulbd9@gmail.com>
2023-10-29 19:15:08 +0200

    feat(contact): new header component

--- src/features/Contact/Contact.tsx
+++ src/features/Contact/Contact.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BlobAnimation } from './components/BlobAnimation';
-import theme from '@/src/styles/tailwind.theme';
+import { ContactHeader } from './components/ContactHeader';
 
 const Contact = () => {
   return (
-    <div>
-      <BlobAnimation stroke="transparent" fill={theme.primary[950]} />
-      <BlobAnimation
-        stroke={theme.black}
-        fill="transparent"
-        time={13}
-        amount={2}
-      />
-    </div>
+    <section className="flex h-full items-center">
+      <ContactHeader />
+    </section>
   );
 };
 
---
+++ src/features/Contact/components/ContactHeader/index.ts
@@ -0,0 +1,1 @@
+export { ContactHeader } from './ContactHeader';
