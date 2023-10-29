aa1aedfcdc2aee6a59f5a82c11d0727420753c7d
Shaul Ben david <Shaulbd9@gmail.com>
2023-10-29 18:53:44 +0200

    feat(blob): new animation component

--- src/features/Contact/Contact.tsx
+++ src/features/Contact/Contact.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BlobAnimation } from './components/BlobAnimation';
+import theme from '@/src/styles/tailwind.theme';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <BlobAnimation stroke="transparent" fill={theme.primary[950]} />
+      <BlobAnimation
+        stroke={theme.black}
+        fill="transparent"
+        time={13}
+        amount={2}
+      />
+    </div>
+  );
 };
 
 export default Contact;
---
+++ src/features/Contact/components/BlobAnimation/index.ts
@@ -0,0 +1,1 @@
+export { BlobAnimation } from './BlobAnimation';
