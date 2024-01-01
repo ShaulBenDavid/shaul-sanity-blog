4c20448d3235b2fc1af881bad1e970dddfb81012
Shaul Ben david <Shaulbd9@gmail.com>
2024-01-01 13:18:00 +0200

    test(card): adding unit test

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "test-utils";
+import { Card } from "./Card";
+
+describe("Card Component", () => {
+  it("renders children and applies custom className", () => {
+    const customClassName = "custom-class";
+    const { getByTestId } = render(
+      <Card className={customClassName}>
+        <span data-testid="child">Card Content</span>
+      </Card>,
+    );
+
+    const cardElement = getByTestId("card-component-test-id");
+    const childElement = getByTestId("child");
+
+    expect(cardElement).toBeInTheDocument();
+    expect(cardElement).toHaveClass(
+      "border-[1px] border-dw-grey bg-bg-grey p-3",
+    );
+    expect(cardElement).toHaveClass(customClassName);
+    expect(childElement).toBeInTheDocument();
+    expect(childElement.textContent).toBe("Card Content");
+  });
+});
