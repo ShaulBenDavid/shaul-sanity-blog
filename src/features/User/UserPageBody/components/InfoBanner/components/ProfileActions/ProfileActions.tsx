443bb635a701fadefa9f5811bf7a60d1f6b5623a
Shaul Ben david <Shaulbd9@gmail.com>
2024-01-11 20:05:03 +0200

    feat(profile): action component

---
+++ src/features/User/UserPageBody/components/InfoBanner/components/ProfileActions/ProfileActions.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, ButtonVariants } from "@/src/components/Button";
+
+//  TODO implement logic
+// interface ProfileActionsProps {
+//   userId: string;
+// }
+
+export const ProfileActions = (): JSX.Element => (
+  <div>
+    <Button variant={ButtonVariants.PRIMARY}>Follow</Button>
+  </div>
+);
