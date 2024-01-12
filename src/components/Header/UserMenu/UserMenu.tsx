57b8c0a508e66bc7a64a2ad2628bd577efb64123
Shaul Ben david <Shaulbd9@gmail.com>
2024-01-12 11:33:41 +0200

    fix(usermenu): adding focus lock

--- src/components/Header/UserMenu/UserMenu.tsx
+++ src/components/Header/UserMenu/UserMenu.tsx
@@ -2,6 +2,7 @@
 
 import React, { useContext, useState } from "react";
 import { MdAdminPanelSettings } from "react-icons/md";
+import ReactFocusLock from "react-focus-lock";
 import { Routes } from "@/src/routes";
 import { PermissionGate, Roles } from "@/src/roles";
 import { AuthContext } from "@/src/context/auth";
@@ -27,40 +28,44 @@ export const UserMenu = (): JSX.Element => {
           label="user menu"
           className="right-0 w-56"
         >
-          <ul role="menu" className="flex flex-col gap-1">
-            <li role="menuitem">
-              <AppLink href={`/@${info.username}`}>
-                <PermissionGate allowedRoles={[Roles.ADMIN]}>
-                  <MdAdminPanelSettings
-                    size={24}
-                    aria-hidden
-                    fill={theme.secondary[950]}
-                  />
-                </PermissionGate>
-                <div className="flex flex-col">
-                  <span>
-                    {info.firstName} {info.lastName}
-                  </span>
-                  <small className="text-sm opacity-70">@{info.username}</small>
-                </div>
-              </AppLink>
-            </li>
-            <hr className="border-secondary-950 opacity-50" />
-            <li role="menuitem">
-              <AppLink href={Routes.READING_LIST}>Reading list</AppLink>
-            </li>
-            <PermissionGate allowedRoles={[Roles.ADMIN, Roles.WRITE]}>
+          <ReactFocusLock>
+            <ul role="menu" className="flex flex-col gap-1">
               <li role="menuitem">
-                <AppLink href={Routes.STUDIO}>Studio</AppLink>
+                <AppLink href={`/@${info.username}`}>
+                  <PermissionGate allowedRoles={[Roles.ADMIN]}>
+                    <MdAdminPanelSettings
+                      size={24}
+                      aria-hidden
+                      fill={theme.secondary[950]}
+                    />
+                  </PermissionGate>
+                  <div className="flex flex-col">
+                    <span>
+                      {info.firstName} {info.lastName}
+                    </span>
+                    <small className="text-sm opacity-70">
+                      @{info.username}
+                    </small>
+                  </div>
+                </AppLink>
               </li>
-            </PermissionGate>
-            <li role="menuitem">
-              <AppLink href={Routes.SETTINGS}>Settings</AppLink>
-            </li>
-            <li role="menuitem">
-              <AppLink onClick={logout}>Logout</AppLink>
-            </li>
-          </ul>
+              <hr className="border-secondary-950 opacity-50" />
+              <li role="menuitem">
+                <AppLink href={Routes.READING_LIST}>Reading list</AppLink>
+              </li>
+              <PermissionGate allowedRoles={[Roles.ADMIN, Roles.WRITE]}>
+                <li role="menuitem">
+                  <AppLink href={Routes.STUDIO}>Studio</AppLink>
+                </li>
+              </PermissionGate>
+              <li role="menuitem">
+                <AppLink href={Routes.SETTINGS}>Settings</AppLink>
+              </li>
+              <li role="menuitem">
+                <AppLink onClick={logout}>Logout</AppLink>
+              </li>
+            </ul>
+          </ReactFocusLock>
         </Dropdown>
       ) : (
         <div className="h-8 w-8 flex-shrink-0 animate-pulse rounded-full bg-secondary-100" />
