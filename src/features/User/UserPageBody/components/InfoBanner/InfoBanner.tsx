a3b98a197568645dcb6c7bf90fa98e50d5654fe5
Shaul Ben david <Shaulbd9@gmail.com>
2024-01-14 19:00:50 +0200

    feat(profile): desktop UI

--- src/features/User/UserPageBody/components/InfoBanner/InfoBanner.tsx
+++ src/features/User/UserPageBody/components/InfoBanner/InfoBanner.tsx
@@ -27,15 +27,15 @@ export const InfoBanner = ({
   details,
 }: InfoBannerProps): JSX.Element => (
   <section aria-label="User Info">
-    <Card className="w-full tb:h-56">
+    <Card className="w-full tb:h-56 md:h-72">
       <header className="flex h-full flex-col gap-6 tb:flex-row tb:justify-between tb:gap-2">
-        <div className="flex w-full flex-row justify-between tb:w-96">
+        <div className="flex w-full flex-row justify-between tb:w-96 md:w-[450px]">
           <UserInfo fullName={fullName} username={username} title={title} />
           <div className="tb:hidden">
             <ActionDropdown />
           </div>
         </div>
-        <div className="flex flex-col gap-6 tb:items-end tb:gap-0">
+        <div className="flex flex-col gap-6 tb:items-end tb:gap-0 md:pb-12 lg:flex-row-reverse lg:items-center lg:pb-0">
           <ProfileActions />
           <UserDetails positions={positions} details={details} />
         </div>
--- src/features/User/UserPageBody/components/InfoBanner/components/UserInfo/UserInfo.tsx
+++ src/features/User/UserPageBody/components/InfoBanner/components/UserInfo/UserInfo.tsx
@@ -16,10 +16,10 @@ export const UserInfo = ({
   <div className="flex flex-row items-center gap-2">
     <Avatar
       name={fullName}
-      className="h-24 w-24 flex-shrink-0 text-6xl tb:h-28 tb:w-28"
+      className="h-24 w-24 flex-shrink-0 text-6xl tb:h-28 tb:w-28 md:h-48 md:w-48 md:text-9xl"
     />
     <div>
-      <h1 className="text-l line-clamp-1 text-ellipsis font-bold capitalize">
+      <h1 className="text-l line-clamp-1 text-ellipsis font-bold capitalize md:text-xl">
         {fullName}
       </h1>
       {title && <p className="line-clamp-2 text-ellipsis leading-5">{title}</p>}
