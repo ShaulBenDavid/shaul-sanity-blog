b1c43bb0dc06c379be2a583f13673ca72b8f1315
Shaul Ben david <Shaulbd9@gmail.com>
2024-02-23 17:16:35 +0200

    feat(post): finish page header

--- src/features/Blog/PostPage/components/PostContent/PostContent.tsx
+++ src/features/Blog/PostPage/components/PostContent/PostContent.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { PortableText } from "@portabletext/react";
 import type { Post as PostType } from "@/src/sanity/types";
-import { Share } from "@/src/components/Share";
 import { RichTextComponents } from "@/src/components/RichTextComponents/RichTextComponents";
 import { PostHeader } from "./components/PostHeader";
 
@@ -16,19 +15,10 @@ export const PostContent = ({ data }: PostContentProps): JSX.Element => (
       createdAt={data._createdAt}
       postImage={data.mainImage}
       authorData={data.author}
+      topics={data.topics}
+      postSlug={data.slug.current}
     />
-    <div className="my-3 sm:px-2 tb:px-4 md:hidden">
-      <Share url={`post/${data.slug.current}`} />
-    </div>
-    <section className="max-w-[70ch]">
-      <div>
-        <h2>{data.description}</h2>
-        <div>
-          {data.topics.map((topic) => (
-            <span key={topic._id}>{topic.title}</span>
-          ))}
-        </div>
-      </div>
+    <section className="max-w-[70ch] sm:px-2 tb:px-4">
       <PortableText value={data.body} components={RichTextComponents} />
     </section>
   </article>
