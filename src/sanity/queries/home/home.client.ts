142378934485d390bdc36acecf844edb67d4b490
Shaul Ben david <Shaulbd9@gmail.com>
2024-02-27 17:27:20 +0200

    fix(build): revalidate

--- src/sanity/queries/home/home.client.ts
+++ src/sanity/queries/home/home.client.ts
@@ -2,7 +2,9 @@ import { client } from "@/src/sanity/sanity.client";
 import { topicsQuery } from "./home.queries";
 import type { GetTopics } from "./home.types";
 
+const REVALIDATE = { next: { revalidate: 86400 } };
+
 export const getTopics = async (): Promise<GetTopics> => {
-  const topics = await client.fetch(topicsQuery);
+  const topics = await client.fetch(topicsQuery, {}, REVALIDATE);
   return topics;
 };
--- src/sanity/queries/post/post.client.ts
+++ src/sanity/queries/post/post.client.ts
@@ -3,17 +3,19 @@ import { allPostsQuery, postQuery, postsQuery } from "./post.queries";
 import type { PostPageResponse } from "./post.types";
 import type { Post } from "../../types";
 
+const REVALIDATE = { next: { revalidate: 86400 } };
+
 export const getPost = async (slug: string): Promise<PostPageResponse> => {
-  const posts = await client.fetch(postQuery, { slug });
+  const posts = await client.fetch(postQuery, { slug }, REVALIDATE);
   return posts;
 };
 
 export const getPosts = async (): Promise<PostPageResponse[]> => {
-  const posts = await client.fetch(postsQuery);
+  const posts = await client.fetch(postsQuery, {}, REVALIDATE);
   return posts;
 };
 
 export const getAllPosts = async (): Promise<Post[]> => {
-  const posts = await client.fetch(allPostsQuery);
+  const posts = await client.fetch(allPostsQuery, {}, REVALIDATE);
   return posts;
 };
