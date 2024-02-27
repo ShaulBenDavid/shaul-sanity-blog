46fca9af0c755767ea9aa12a7981d90596c368d0
Shaul Ben david <Shaulbd9@gmail.com>
2024-02-27 16:12:30 +0200

    feat(metatag): dynamic head tags to post page

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { groq } from "next-sanity";
 import { client } from "@/src/sanity/sanity.client";
 import type { Post as PostType } from "@/src/sanity/types";
 import { PostPage } from "@/src/features/Blog/PostPage";
 import { type PostPageResponse, getPost } from "@/src/sanity/queries/post";
+import { websiteUrl } from "@/src/constants";
 
-type Props = {
+type PostProps = {
   params: {
     slug: string;
   };
@@ -28,7 +30,22 @@ export async function generateStaticParams() {
   }));
 }
 
-const Post = async ({ params: { slug } }: Props) => {
+export async function generateMetadata({
+  params: { slug },
+}: PostProps): Promise<Metadata> {
+  const { title, description, author }: PostPageResponse = await getPost(slug);
+
+  return {
+    title,
+    description,
+    authors: {
+      name: author.username,
+      url: `${websiteUrl}@${author.username}`,
+    },
+  };
+}
+
+const Post = async ({ params: { slug } }: PostProps) => {
   const post: PostPageResponse = await getPost(slug);
 
   if (!post) {
