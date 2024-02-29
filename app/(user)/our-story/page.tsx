d62f577a8ea107975060742e50dfadb92633a0e6
Shaul Ben david <Shaulbd9@gmail.com>
2024-02-29 16:05:13 +0200

    fix(about): background position

--- app/(user)/our-story/page.tsx
+++ app/(user)/our-story/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const OurStory = () => (
-  <div className="polka-bg shadow-inset-blur mx-[calc(-50vw+50%)] flex min-h-screen w-screen justify-center bg-fixed py-6">
-    <section className="mx-2 flex h-fit max-w-[70ch] flex-col gap-4 rounded-md bg-white p-6 shadow-container sm:m-0 sm:px-2 tb:px-6">
+  <div className="polka-bg shadow-inset-blur mx-[calc(-50vw+50%)] flex min-h-screen w-screen justify-center bg-fixed py-10">
+    <section className="mx-2 flex h-fit max-w-[70ch] flex-col gap-4 rounded-md bg-white p-2 shadow-container sm:m-0 sm:p-4 tb:p-6">
       <header>
         <h1 className="text-2xl font-bold md:text-6xl">
           Our Story <span className="text-primary-900">DevWizard</span>
